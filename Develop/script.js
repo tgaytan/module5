76ad627ee906b6371fa471d81164db38d0e84d3a
tgaytan <tgaytan14@utexas.edu>
2023-09-09 18:43:22 -0500

    added code that changes color of block based on time

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,49 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
 
-// let now = dayjs('2023-09-09').format('dddd');
+// getting current time from dayjs() and storing in variables
+const year = dayjs().year();
+const month = dayjs().month() + 1;
+const date = dayjs().date();
+const hour = 9 // dayjs().hour();
+const currentDay = dayjs(year + "-" + month + "-" + date).format('dddd, MMMM DD'); //displays today's date in format like "Saturday, September 09"
+// console.log(hour);
+
 // let now = dayjs();
 // console.log(now);
 
-let year = dayjs().year();
-let month = dayjs().month() + 1;
-let date = dayjs().date();
+//creating variables that control the start hour and end hour of the day
+const startHour = 9; //9am
+const endHour = 17;  //5pm
+const workHours = endHour - startHour; //number of hours in working day
 
-  let currentDay = dayjs(year + "-" + month + "-" + date).format('dddd, MMMM DD');
-  console.log(currentDay);
+$(function () {
+  // populating array with elements that have the class like hour-10
+  const timeBlocks = [];
+  for (let i = startHour; i <= endHour; i++) {
+    // console.log('#hour-' + i);
+    timeBlocks[i] = $('#hour-' + i);
+  }
 
-  let currentDayEl = $('#currentDay');
+  //  for loop that updates the past/present/future class in element based on "hour" variable
+  for (let i = startHour; i <= endHour; i++) {
+    // gets the current class from element and then removes it
+    let currentClass = timeBlocks[i].attr('class').slice(15); // need to use slice since there are multiple classes
+    timeBlocks[i].removeClass(currentClass); 
+
+    // updates class in element to have status of past, present, future
+    if (i < hour) {
+      timeBlocks[i].addClass('past');
+    } else if (i === hour) {
+      timeBlocks[i].addClass('present');
+    } else if (i > hour) {
+      timeBlocks[i].addClass('future');
+    }
+  }
+
+  // prints the current date to the web page
+  const currentDayEl = $('#currentDay');
   currentDayEl.text(currentDay);
 
 
